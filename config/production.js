65f80b57a02fe4ef175083ee5c67289355aa1b56
Joel Wietelmann <jwietelmann@gmail.com>
2013-02-01 09:52:12 -0600

    adding production.js config

---
+++ config/production.js
@@ -0,0 +1,20 @@
+exports.loggerFormat = 'dev';
+exports.useErrorHandler = true;
+exports.mongodb = ENV.MONGOHQ_URL;
+exports.sessionSecret = 'your secret here';
+
+exports.enableGuestLogin = true;
+exports.enableEmailLogin = true;
+exports.twitter = {
+  consumerKey: 'my consumer key',
+  consumerSecret: 'my consumer secret'
+};
+exports.facebook = {
+  clientID: 'my client id',
+  clientSecret: 'my client secret',
+  callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback'
+};
+
+exports.socketIo = {
+  useLongPolling: true // Set to 'true' for Heroku deployment, 'false' for platforms that support WebSockets
+};
